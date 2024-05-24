967025c8a1bfebf33aea79e7cea75b4ce8e9db45
nitandrei1058 <152653215+nitandrei1058@users.noreply.github.com>
2024-05-24 11:13:00 +0300

    Updated TASK 3 with common-group

--- feed.c
+++ feed.c
@@ -55,12 +55,73 @@ void view_profile(char *name, tree_t *posts_tree)
 void friends_repost(char *name, int id, int **friendship, tree_t *posts_tree)
 {
 	int user_id = get_user_id(name);
-	node_t *post_node = ((post_t *)(search_post(posts_tree, id)->data))->events->root;
+	node_t *post_node =
+		((post_t *)(search_post(posts_tree, id)->data))->events->root;
 	for (int i = 0; i < post_node->sons_count; i++) {
 		post_t *post = (post_t *)post_node->sons[i]->data;
 		if (friendship[user_id][post->user_id])
 			printf("%s\n", get_user_name(post->user_id));
 	}
+}
+
+void clique_bktk(int **friendship, uint16_t *friends, uint16_t level,
+				 uint16_t friends_size, uint8_t *clique, uint8_t *max_clique,
+				 int *max_clique_size, int *clique_size)
+{
+	if (*clique_size > *max_clique_size) {
+		*max_clique_size = *clique_size;
+		memcpy(max_clique, clique, MAX_PEOPLE * sizeof(uint8_t));
+	}
+
+	for (int i = level; i < friends_size; i++) {
+		uint8_t good = 1;
+		for (int j = 0; j < MAX_PEOPLE; j++) {
+			if (clique[j] && !friendship[friends[i]][j]) {
+				good = 0;
+				break;
+			}
+		}
+		if (good) {
+			clique[friends[i]] = 1;
+			(*clique_size)++;
+			clique_bktk(friendship, friends, i + 1, friends_size, clique,
+						max_clique, max_clique_size, clique_size);
+			clique[friends[i]] = 0;
+			(*clique_size)--;
+		}
+	}
+}
+
+void common_group(char *name, int **friendship)
+{
+	int user_id = get_user_id(name);
+
+	uint8_t *clique = calloc(MAX_PEOPLE, sizeof(uint8_t));
+	uint8_t *max_clique = calloc(MAX_PEOPLE, sizeof(uint8_t));
+	int max_clique_size = 1;
+	int clique_size = 1;
+	clique[user_id] = 1;
+	max_clique[user_id] = 1;
+
+	// trebuie sa fac un vector cu toti ratatii aia si sa iau size-ul gen
+
+	uint16_t *friends = calloc(MAX_PEOPLE, sizeof(uint16_t));
+	uint16_t friends_size = 0;
+	for (int i = 0; i < MAX_PEOPLE; i++)
+		if (friendship[user_id][i])
+			friends[friends_size++] = i;
+
+	clique_bktk(friendship, friends, 0, friends_size, clique, max_clique,
+				&max_clique_size, &clique_size);
+
+	printf("The closest friend group of %s is:\n", name);
+	for (int i = 0; i < MAX_PEOPLE; i++) {
+		if (max_clique[i])
+			printf("%s\n", get_user_name(i));
+	}
+	free(clique);
+	free(max_clique);
+	free(friends);
 }
 
 void handle_input_feed(char *input, int **friendship, tree_t *posts)
@@ -82,8 +143,9 @@ void handle_input_feed(char *input, int **friendship, tree_t *posts)
 		char *name = strtok(NULL, "\n ");
 		int id = atoi(strtok(NULL, "\n "));
 		friends_repost(name, id, friendship, posts);
-	} else if (!strcmp(cmd, "common-groups")) {
-		(void)cmd;
+	} else if (!strcmp(cmd, "common-group")) {
+		char *name = strtok(NULL, "\n ");
+		common_group(name, friendship);
 	}
 
 	free(commands);
